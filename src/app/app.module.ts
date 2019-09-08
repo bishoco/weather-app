534556ab18a42f953485ef15fa0d27bf478587a8
Corey Bishop <bishoco@gmail.com>
2019-09-08 09:01:22 -0400

    added angular material and current-weather-display component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CurrentWeatherDisplayComponent } from './current-weather-display/current-weather-display.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrentWeatherDisplayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/current-weather-display/current-weather-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'current-weather-display',
+  templateUrl: './current-weather-display.component.html',
+  styleUrls: ['./current-weather-display.component.css']
+})
+export class CurrentWeatherDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
