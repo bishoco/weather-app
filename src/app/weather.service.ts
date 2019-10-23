c62379a06ba0b656186149f16ecf3b01db9b299c
Corey Bishop <bishoco@gmail.com>
2019-10-23 08:21:53 -0400

    Added commenting to some of the services

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -4,10 +4,17 @@ import { Subject } from 'rxjs';
 import { WeatherCondition } from './models/weather-condition.enum';
 import { Forecast } from './models/forecast.interface';
 
+/*
+This class provides observables for the forecast data so they can be
+passed easily between components.
+Also, a method to determine Weather Condition.
+*/
+
 @Injectable({
   providedIn: 'root'
 })
-export class WeatherService {  
+export class WeatherService {
+  //The observables are used by serveral components and services to pass this info around
   forecastSource = new Subject<Array<Forecast>>();
   forecastHourlySource = new Subject<Array<Forecast>>();
 
@@ -16,6 +23,13 @@ export class WeatherService {
 
   constructor() { }
 
+  /*
+    Returns a WeatherCondition enum based on the weather condition string passed in.
+    I'm doing a pretty basic word lookup to determine the weather condition.
+    The NWS API returns a weather condition string that has a lot of variability
+    and natural language, so I did my best to determine weather condition based
+    on some key words.
+  */
   determineCondition (condition : String ) : WeatherCondition {
     if (condition.toLowerCase().includes('storms'))
       return WeatherCondition.Stormy;
